db1807d186fafdfc0db914d73f1b302ee6ad7010
mariajosetapia <37387923+mariajosetapia@users.noreply.github.com>
2018-03-29 01:39:56 +0200

    Update hola_.js

--- hola_.js
+++ hola_.js
@@ -6,18 +6,23 @@ var alfa;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
 }
-
 
 function draw() {
   background(0);  
    
-stroke(255);
-strokeWeight(18);
+
 
   nPuntos = int(map(mouseX, 0, width, 3, 100));
   nCirculos = int(map(mouseY, 0, height, 3, 8));
+  if (es_par(nCirculos)){
+	  stroke(255);
+	strokeWeight(18);
+  }else{
+	  stroke(255,0,0);
+	strokeWeight(18);
+  }
+  
   alfa = TWO_PI/nPuntos;
   push();
   translate(width/2,height/2);
@@ -33,4 +38,12 @@ line(j*rCirculo*cos(i*alfa), j*rCirculo*sin(i*alfa),j*rCirculo*cos((i+1)*alfa), 
 
   }
   pop();
+}
+
+function es_par(cantidad){
+	if(cantidad % 2 ==0){
+		return true;
+	}else{
+		false;
+	}
 }
